801ec9b8019d0540bca00aa84aa74d7af2fe4c5c
Scouter456 <markveenstra14@gmail.com>
2024-06-28 20:25:29 +0200

    Fix mobs running in place and correct random states using goal

---
+++ src/main/java/com/peeko32213/seafarer/common/entity/StatedAnimal.java
@@ -0,0 +1,64 @@
+package com.peeko32213.seafarer.common.entity;
+
+import com.peeko32213.seafarer.common.entity.misc.state.IStateAction;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.network.syncher.EntityDataSerializers;
+import net.minecraft.network.syncher.SynchedEntityData;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
+
+public abstract class StatedAnimal extends Animal implements IStateAction {
+    private static final EntityDataAccessor<Boolean> PERFORMING_ACTION = SynchedEntityData.defineId(StatedAnimal.class, EntityDataSerializers.BOOLEAN);
+
+    protected StatedAnimal(EntityType<? extends Animal> pEntityType, Level pLevel) {
+        super(pEntityType, pLevel);
+    }
+
+
+
+    @Override
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(PERFORMING_ACTION, false);
+    }
+
+    public boolean getBooleanState(EntityDataAccessor<Boolean> pKey) {
+        return this.entityData.get(pKey);
+    }
+
+    public void setBooleanState(EntityDataAccessor<Boolean> pKey, boolean state) {
+        this.entityData.set(pKey, state);
+    }
+
+
+    @Override
+    public void travel(Vec3 pTravelVector) {
+        if (this.getPerformingAction()) {
+            if (this.getNavigation().getPath() != null) {
+                this.getNavigation().stop();
+            }
+            pTravelVector = Vec3.ZERO;
+        }
+        super.travel(pTravelVector);
+    }
+
+    public boolean getPerformingAction() {
+        return this.entityData.get(PERFORMING_ACTION);
+    }
+
+    public void setPerformingAction(boolean action) {
+        this.entityData.set(PERFORMING_ACTION, action);
+    }
+    @Override
+    public boolean getAction() {
+        return getPerformingAction();
+    }
+
+    @Override
+    public void setAction(boolean action) {
+        setPerformingAction(action);
+    }
+}
